1a3c923b8d729f7dec9342455d48816c79e887de
Tinkermannn <wilmansaragih2@gmail.com>
2024-11-22 19:57:56 +0700

    Delete unused class

--- obj/Debug/net8.0/OOP_PROJECT.AssemblyInfo.cs
+++ obj/Debug/net8.0/OOP_PROJECT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP_PROJECT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb36f6ecf9fd114316113d1089cb94ac15053861")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP_PROJECT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP_PROJECT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
